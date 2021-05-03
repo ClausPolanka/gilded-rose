7101be628acd27bb69e571343f67e39366928629
Ana-Maria Patrascu <e1129771@student.tuwien.ac.at>
2021-05-03 09:45:17 +0200

    add tests for basic item

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,12 +6,63 @@ import org.junit.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
+    fun `basic item should decrease quality by 1 when sellIn is positive`() {
+        val items = arrayOf<Item>(Item("Basic Item", 1, 5))
         val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
 
+        app.updateQuality()
+
+        assertEquals("Basic Item", app.items[0].name)
+        assertEquals(0, app.items[0].sellIn)
+        assertEquals(4, app.items[0].quality)
+    }
+
+    @Test
+    fun `basic item should decrease quality by 2 when sellIn is lte 0`() {
+        val items = arrayOf<Item>(Item("Basic Item", 0, 5))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals("Basic Item", app.items[0].name)
+        assertEquals(-1, app.items[0].sellIn)
+        assertEquals(3, app.items[0].quality)
+    }
+
+    @Test
+    fun `basic item should not change to negative quality when sellIn is positive`() {
+        val items = arrayOf<Item>(Item("Basic Item", 1, 0))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals("Basic Item", app.items[0].name)
+        assertEquals(0, app.items[0].sellIn)
+        assertEquals(0, app.items[0].quality)
+    }
+
+    @Test
+    fun `basic item should change to negative quality when sellIn is lte 0`() {
+        val items = arrayOf<Item>(Item("Basic Item", 0, 1))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals("Basic Item", app.items[0].name)
+        assertEquals(-1, app.items[0].sellIn)
+        assertEquals(0, app.items[0].quality)
+    }
+
+    @Test
+    fun `basic item should not change quality when quality is min and sellIn is lte 0`() {
+        val items = arrayOf<Item>(Item("Basic Item", 0, 0))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals("Basic Item", app.items[0].name)
+        assertEquals(-1, app.items[0].sellIn)
+        assertEquals(0, app.items[0].quality)
     }
 
 }
