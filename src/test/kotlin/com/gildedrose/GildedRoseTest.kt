1d7c304b129ef084c2af32d7eb5ba2ae3ce6b1b3
Ana-Maria Patrascu <e1129771@student.tuwien.ac.at>
2021-05-03 10:34:28 +0200

    add tests for all the categories

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -2,6 +2,7 @@ package com.gildedrose
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import kotlin.math.exp
 
 internal class GildedRoseTest {
 
@@ -12,10 +13,10 @@ internal class GildedRoseTest {
 
         app.updateQuality()
 
-        assertEquals("Item name", "Basic Item", app.items[0].name)
-        assertEquals("Item sellIn", 0, app.items[0].sellIn)
-        assertEquals("Item quality", 4, app.items[0].quality)
+        assertEqualsItems(app.items[0], Item("Basic Item", 0, 4))
     }
+
+
 
     @Test
     fun `basic item should decrease quality by 2 when sellIn is lte 0`() {
@@ -24,9 +25,7 @@ internal class GildedRoseTest {
 
         app.updateQuality()
 
-        assertEquals("Item name", "Basic Item", app.items[0].name)
-        assertEquals("Item sellIn", -1, app.items[0].sellIn)
-        assertEquals("Item quality", 3, app.items[0].quality)
+        assertEqualsItems(app.items[0], Item("Basic Item", -1, 3))
     }
 
     @Test
@@ -36,9 +35,7 @@ internal class GildedRoseTest {
 
         app.updateQuality()
 
-        assertEquals("Item name", "Basic Item", app.items[0].name)
-        assertEquals("Item sellIn", 0, app.items[0].sellIn)
-        assertEquals("Item quality", 0, app.items[0].quality)
+        assertEqualsItems(app.items[0], Item("Basic Item", 0, 0))
     }
 
     @Test
@@ -48,9 +45,7 @@ internal class GildedRoseTest {
 
         app.updateQuality()
 
-        assertEquals("Item name", "Basic Item", app.items[0].name)
-        assertEquals("Item sellIn", -1, app.items[0].sellIn)
-        assertEquals("Item quality", 0, app.items[0].quality)
+        assertEqualsItems(app.items[0], Item("Basic Item", -1, 0))
     }
 
     @Test
@@ -60,10 +55,173 @@ internal class GildedRoseTest {
 
         app.updateQuality()
 
-        assertEquals("Item name", "Basic Item", app.items[0].name)
-        assertEquals("Item sellIn",-1, app.items[0].sellIn)
-        assertEquals("Item quality", 0, app.items[0].quality)
+        assertEqualsItems(app.items[0], Item("Basic Item", -1, 0))
     }
 
+    @Test
+    fun `aged brie item should increase quality by 1 when sellIn is gt 0`() {
+        val items = arrayOf<Item>(Item("Aged Brie", 1, 3))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Aged Brie", 0, 4))
+    }
+
+    @Test
+    fun `aged brie item should increase quality by 2 when sellIn is lte 0`() {
+        val items = arrayOf<Item>(Item("Aged Brie", 0, 3))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Aged Brie", -1, 5))
+    }
+
+    @Test
+    fun `aged brie item should not increase quality more than max when sellIn is gt 0`() {
+        val items = arrayOf<Item>(Item("Aged Brie", 1, 50))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Aged Brie", 0, 50))
+    }
+
+    @Test
+    fun `aged brie item should not increase quality more than max when sellIn is lte 0`() {
+        val items = arrayOf<Item>(Item("Aged Brie", 0, 49))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Aged Brie", -1, 50))
+    }
+
+    @Test
+    fun `aged brie item should not increase quality when sellIn is lte 0`() {
+        val items = arrayOf<Item>(Item("Aged Brie", 0, 50))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Aged Brie", -1, 50))
+    }
+
+    @Test
+    fun `sulfuras item should not change quality nor sellIn`() {
+        val items = arrayOf<Item>(Item("Sulfuras, Hand of Ragnaros", 0, 80))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Sulfuras, Hand of Ragnaros", 0, 80))
+    }
+
+    @Test
+    fun `backstage passes should increase quality by 1 when sellIn gt 10`() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 11, 5))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Backstage passes to a TAFKAL80ETC concert", 10, 6))
+    }
+
+    @Test
+    fun `backstage passes should increase quality by 2 when sellIn lte 10 and gt 5`() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 10, 5))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Backstage passes to a TAFKAL80ETC concert", 9, 7))
+    }
+
+    @Test
+    fun `backstage passes should increase quality by 3 when sellIn lte 5 and gt 0`() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 5, 5))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Backstage passes to a TAFKAL80ETC concert", 4, 8))
+    }
+
+    @Test
+    fun `backstage passes should make quality 0 when sellIn is 0`() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 0, 5))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Backstage passes to a TAFKAL80ETC concert", -1, 0))
+    }
+
+    @Test
+    fun `backstage passes should not increase quality when quality is max and sellIn gt 10`() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 11, 50))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Backstage passes to a TAFKAL80ETC concert", 10, 50))
+    }
+
+    @Test
+    fun `backstage passes should not increase quality more than max and sellIn lte 10 and gt 5`() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 10, 49))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Backstage passes to a TAFKAL80ETC concert", 9, 50))
+    }
+
+    @Test
+    fun `backstage passes should not increase quality when quality is max and sellIn lte 10 and gt 5`() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 10, 50))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Backstage passes to a TAFKAL80ETC concert", 9, 50))
+    }
+
+    @Test
+    fun `backstage passes should not increase quality when quality is max and sellIn lte 5 and gt 0`() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 5, 50))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Backstage passes to a TAFKAL80ETC concert", 4, 50))
+    }
+
+    @Test
+    fun `backstage passes should not increase quality not more than max when sellIn lte 5 and gt 0`() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 5, 49))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Backstage passes to a TAFKAL80ETC concert", 4, 50))
+    }
+
+    @Test
+    fun `backstage passes should not increase quality not more than max when sellIn lte 5 and gt 0 and quality is max - 2`() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 5, 48))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEqualsItems(app.items[0], Item("Backstage passes to a TAFKAL80ETC concert", 4, 50))
+    }
+
+    private fun assertEqualsItems(actual: Item, expected: Item) {
+        assertEquals("Item name", expected.name, actual.name)
+        assertEquals("Item sellIn", expected.sellIn, actual.sellIn)
+        assertEquals("Item quality", expected.quality, actual.quality)
+    }
 }
 
